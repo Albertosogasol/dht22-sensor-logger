f0adabf369e50c5ec97909b95d1a59e06ac9c921
Albertosogasol <gonzagaalberto@hotmail.com>
2024-12-02 17:07:58 +0100

    Added allowed values in humidity and round values in temp and hum

--- dht22_sensor.py
+++ dht22_sensor.py
@@ -95,13 +95,12 @@ def read_sensor(pin):
     for _ in range(5):  # Intentar hasta 5 veces
         humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, pin)
         if humidity is not None and temperature is not None:
-            return temperature, humidity
+            return round(temperature, 2), round(humidity, 2)
         else:
             logging.warning(f"Fallo en la lectura del sensor en el pin {pin}. Reintentando...")
         time.sleep(1)  # Esperar 1 segundo antes de reintentar
     logging.error(f"Fallo en la lectura del sensor en el pin {pin} después de varios intentos.")
     return None, None
-
 
 # Insertar datos en DB
 def insert_data(conn, location_name, temperature, humidity, sensor_type):
@@ -119,6 +118,22 @@ def insert_data(conn, location_name, temperature, humidity, sensor_type):
     except sqlite3.Error as e:
         logging.error(f"Error al insertar datos en la base de datos: {e}")
 
+# Función para obtener medición válida
+def get_valid_measurement(pin, sensor_location):
+    """Obtenemos una medición válida de los sensores con reintentos si es necesario"""
+    for attempt in range(5):
+        temperature, humidity = read_sensor(pin)
+        
+        if humidity is not None and 0 <= humidity <= 100:
+            return temperature, humidity
+        if attempt < 4:  # Solo esperar 10s si no es el último intento
+            logging.warning(f"Lectura incorrecta. Reintentando en 10 segundos... (Intento {attempt + 1}/5)")
+            time.sleep(10)
+    
+    # Si no se obtuvo lectura válida después de 5 intentos
+    logging.error(f"No se pudo obtener una medición válida de {sensor_location} después de 5 intentos.")
+    return None, None
+
 # Funcion principal
 def main():
     load_configuration()
@@ -126,19 +141,17 @@ def main():
     # Establecer conexión con la base de datos
     conn = create_db_connection()
     if conn:
-        create_table(conn)  # Asegurarse de que la tabla existe
+        create_table(conn)  # Asegurarse de que la tabla exista
 
         # Leer los datos del sensor exterior y guardarlos en la base de datos
-        temperature, humidity = read_sensor(outside_pin)
+        temperature, humidity = get_valid_measurement(outside_pin, "exterior")
         if temperature and humidity:
-            sensor_location = "exterior"
-            insert_data(conn, location_name, temperature, humidity, sensor_location)
+            insert_data(conn, location_name, temperature, humidity, "exterior")
     
         # Leer los datos del sensor interior y guardarlos en la base de datos
-        temperature, humidity = read_sensor(inside_pin)
+        temperature, humidity = get_valid_measurement(inside_pin, "interior")
         if temperature and humidity:
-            sensor_location = "interior"
-            insert_data(conn, location_name, temperature, humidity, sensor_location)
+            insert_data(conn, location_name, temperature, humidity, "interior")
 
 if __name__ == "__main__":
     main()
